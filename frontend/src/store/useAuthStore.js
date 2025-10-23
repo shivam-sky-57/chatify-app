5a3730bbcb80505234c2937c409a98de1e8bd9be
shivam-sky-57 <shivamkumar7965@gmail.com>
2025-10-23 11:10:30 +0530

    login & logout implemented

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -31,5 +31,30 @@ export const useAuthStore = create((set) => ({
         } finally {
             set({ isSigninUp: false })
         }
+    },
+
+    login: async (data) => {
+        set({ isLoggingUp: true });
+        try {
+            const res = await axiosInstance.post("/auth/login", data);
+            set({ authUser: res.data});
+
+            toast.success("Logged in successfully!");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isSigninUp: false })
+        }
+    },
+
+    logout: async () => {
+        try {
+            await axiosInstance.post("auth/logout");
+            set({ authUser: null});
+            toast.success("Logged out successfully");
+        } catch (error) {
+            toast.error("Error logging out");
+            console.log("Logout error:", error);
+        }
     }
 }))
