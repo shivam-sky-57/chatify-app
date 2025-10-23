9adf12c51078244266949bc571a234b1d8129ef1
shivam-sky-57 <shivamkumar7965@gmail.com>
2025-10-23 11:21:44 +0530

    fix typo

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,6 +6,7 @@ export const useAuthStore = create((set) => ({
     authUser: null,
     isCheckingAuth: true,
     isSigningUp: false,
+    isLoggingIn: false,
 
     checkAuth: async () => {
         try {
@@ -34,7 +35,7 @@ export const useAuthStore = create((set) => ({
     },
 
     login: async (data) => {
-        set({ isLoggingUp: true });
+        set({ isLoggingIn: true });
         try {
             const res = await axiosInstance.post("/auth/login", data);
             set({ authUser: res.data});
@@ -43,7 +44,7 @@ export const useAuthStore = create((set) => ({
         } catch (error) {
             toast.error(error.response.data.message);
         } finally {
-            set({ isSigninUp: false })
+            set({ isSigningUp: false })
         }
     },
 
