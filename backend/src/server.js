d4905b49f2e6b74a649d0b7dceaba25367d2696d
shivam-sky-57 <shivamkumar7965@gmail.com>
2025-10-24 21:33:35 +0530

    send message func. implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json()) // req.body
+app.use(express.json( {limit: "5mb"})) // req.body
 app.use(cors({origin: ENV.CLIENT_URL, credentials: true}))
 app.use(cookieParser())
 
