b1b19ef9cf37304b26f586e55c871a541e2fa611
shivam-sky-57 <shivamkumar7965@gmail.com>
2025-10-24 17:18:23 +0530

    sidebar completed

--- frontend/src/components/ContactList.jsx
+++ frontend/src/components/ContactList.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import UsersLoadingSkeleton from "./UsersLoadingSkeleton";
 
 function ContactList() {
+  const { getAllContacts, allContacts, setSelectedUser, isUsersLoading} = useChatStore();
+
+  useEffect(() => {
+    getAllContacts();
+  }, [getAllContacts]);
+
+  if (isUsersLoading) return <UsersLoadingSkeleton />;
+
   return (
-    <div>
-      
-    </div>
+    <>
+        {allContacts.map((contact) => (
+            <div
+                key={contact.id}
+                className="bg-cyan-500/10 p-4 rounded-lg cursor-pointer hover:bg-cyan-500/20 transition-colors"
+                onClick={() => setSelectedUser(contact)}
+            >
+              <div className="flex itmes-center gap-3">
+                <div className={`avatar online`}>
+                  <div className="size-12 rounded-full">
+                    <img src={contact.profilePic || '/avatar.png'} />
+                  </div>
+                </div>
+                <h4 className="text-slate-200 font-medium truncate">{contact.fullName}</h4>
+              </div>
+            </div>
+        ))}
+    </>
   )
 }
 
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -11,7 +11,7 @@ export const useChatStore = create((set,get) => ({
     selectedUser:null,
     isUsersLoading:false,
     isMessagesLoading: false,
-    isSoundEnabled: localStorage.getItem("isSoundEnabled") === true,
+    isSoundEnabled: JSON.parse(localStorage.getItem("isSoundEnabled")) === true,
 
     toggleSound: () => {
         localStorage.setItem("isSoundEnabled", !get().isSoundEnabled)
